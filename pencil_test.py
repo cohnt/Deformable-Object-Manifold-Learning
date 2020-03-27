5bfb7875ee8d2cdcbe3d4c98ec2063d6138c99cb
Thomas Cohn <cohnt@umich.edu>
2020-03-27 15:12:59 -0400

    Read the video frame-by-frame.

--- pencil_test.py
+++ pencil_test.py
@@ -4,3 +4,16 @@ import cv2
 num_points_to_track = 10
 x_coord_start = 1920 / 4
 x_coord_stop = (1920 / 4) * 3
+
+cap = cv2.VideoCapture("data/colored_pencil.mp4")
+if not cap.isOpened():
+	print "Error opening video stream or file"
+
+while cap.isOpened():
+	ret, frame = cap.read()
+	if ret:
+		pass
+	else:
+		break
+
+cap.release()
