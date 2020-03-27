850ef785aabd3596be3c270f358072f4f2fb44cf
Thomas Cohn <cohnt@umich.edu>
2020-03-27 15:17:03 -0400

    Get the x and y coords of the pencil points (getRedHeight doesn't do anything yet).

--- pencil_test.py
+++ pencil_test.py
@@ -5,14 +5,21 @@ num_points_to_track = 10
 x_coord_start = 1920 / 4
 x_coord_stop = (1920 / 4) * 3
 
+manifold_data = []
+
 cap = cv2.VideoCapture("data/colored_pencil.mp4")
 if not cap.isOpened():
 	print "Error opening video stream or file"
 
+def getRedHeight(image, x):
+	return 0
+
 while cap.isOpened():
 	ret, frame = cap.read()
 	if ret:
-		pass
+		x_coords = np.linspace(x_coord_start, x_coord_stop, num=num_points_to_track, endpoint=True, dtype=int)
+		y_coords = np.array([getRedHeight(frame, x) for x in x_coords])
+		manifold_data.append(y_coords)
 	else:
 		break
 
