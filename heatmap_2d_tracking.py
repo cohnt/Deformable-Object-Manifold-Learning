4a71b4c1be1780286ccf69b35758c578e3f1fc6d
Thomas Cohn <cohnt@umich.edu>
2020-05-05 18:53:37 -0400

    When choosing color, use a different scaling so it's more clear.

--- heatmap_2d_tracking.py
+++ heatmap_2d_tracking.py
@@ -273,7 +273,7 @@ while cap.isOpened():
 		num_particles = 200
 		exploration_factor = 0.75
 		particles = [Particle() for i in range(num_particles)]
-		disp_thresh = 0.25
+		disp_thresh = 0.05
 
 		while True:
 			# Weight particles
@@ -289,6 +289,7 @@ while cap.isOpened():
 				w = p.raw_weight / max_weight
 				p.normalized_weight = w
 				normalized_weights.append(w)
+			max_normalized_weight = np.max(normalized_weights)
 
 			# Display
 			fig, axes = plt.subplots(2, 1)
@@ -296,9 +297,9 @@ while cap.isOpened():
 			axes[1].imshow(normalized_red_matrix, cmap="gray")
 			for p in particles:
 				if p.normalized_weight > 0:
-					axes[0].plot(p.points.T[:,0], p.points.T[:,1], c=plt.cm.cool(p.normalized_weight), linewidth=2)
+					axes[0].plot(p.points.T[:,0], p.points.T[:,1], c=plt.cm.cool(p.normalized_weight / max_normalized_weight), linewidth=2)
 					if p.normalized_weight > disp_thresh:
-						axes[1].plot(p.points.T[:,0], p.points.T[:,1], c=plt.cm.cool(p.normalized_weight), linewidth=2)
+						axes[1].plot(p.points.T[:,0], p.points.T[:,1], c=plt.cm.cool(p.normalized_weight / max_normalized_weight), linewidth=2)
 
 			axes[0].set_xlim((0,1920))
 			axes[0].set_ylim((1080,0))
