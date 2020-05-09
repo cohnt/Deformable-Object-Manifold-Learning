3b61855c1ed0bc9bda847598d113a4b18f77d2bc
Thomas Cohn <cohnt@umich.edu>
2020-05-08 21:39:10 -0400

    Adjust the radius of points in 2d and 3d.

--- manifold_figure.py
+++ manifold_figure.py
@@ -10,9 +10,12 @@ X, color = datasets.make_s_curve(n_points, random_state=0)
 n_neighbors = 8
 n_components = 2
 
+p_rad_3d = 7
+p_rad_2d = 10
+
 fig = plt.figure()
 ax = fig.add_subplot(111, projection="3d")
-ax.scatter(X[:,0], X[:,1], X[:,2], c=color, cmap=plt.cm.Spectral)
+ax.scatter(X[:,0], X[:,1], X[:,2], c=color, cmap=plt.cm.Spectral, s=p_rad_3d**2)
 ax.view_init(4, -72)
 plt.show()
 
@@ -21,5 +24,5 @@ plt.show()
 Y = manifold.Isomap(n_neighbors, n_components).fit_transform(X)
 fig = plt.figure()
 ax = fig.add_subplot(111)
-ax.scatter(Y[:,0], Y[:,1], c=color, cmap=plt.cm.Spectral)
+ax.scatter(Y[:,0], Y[:,1], c=color, cmap=plt.cm.Spectral, s=p_rad_2d**2)
 plt.show()
