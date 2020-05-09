8f006f5dc51bccbb0e8806e34fc61f2438c90435
Thomas Cohn <cohnt@umich.edu>
2020-05-08 21:36:25 -0400

    Display a pointset sampled from the S surface.

---
+++ manifold_figure.py
@@ -0,0 +1,17 @@
+import numpy as np
+from sklearn import datasets, manifold
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+# Dataset 3D plot
+
+n_points = 250
+X, color = datasets.make_s_curve(n_points, random_state=0)
+n_neighbors = 8
+n_components = 2
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection="3d")
+ax.scatter(X[:,0], X[:,1], X[:,2], c=color, cmap=plt.cm.Spectral)
+ax.view_init(4, -72)
+plt.show()
