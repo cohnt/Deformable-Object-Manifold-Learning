52b49d155aabc943485be576320e383449bd03cd
Thomas Cohn <cohnt@umich.edu>
2020-05-08 21:48:23 -0400

    Add the 3d triangle plot.

--- manifold_figure.py
+++ manifold_figure.py
@@ -7,7 +7,7 @@ from scipy.spatial import Delaunay
 # Dataset 3D plot
 
 n_points = 250
-X, color = datasets.make_s_curve(n_points, random_state=0)
+X, color = datasets.make_s_curve(n_points, random_state=1)
 n_neighbors = 8
 n_components = 2
 
@@ -39,9 +39,24 @@ ax = fig.add_subplot(111)
 
 for simplex in tri.simplices:
 	points = Y[simplex]
-	ax.plot(points[[0,1]][:,0], points[[0,1]][:,1], c="black")
-	ax.plot(points[[0,2]][:,0], points[[0,2]][:,1], c="black")
-	ax.plot(points[[1,2]][:,0], points[[1,2]][:,1], c="black")
+	ax.plot(points[[0,1]][:,0], points[[0,1]][:,1], c="black", zorder=1)
+	ax.plot(points[[0,2]][:,0], points[[0,2]][:,1], c="black", zorder=1)
+	ax.plot(points[[1,2]][:,0], points[[1,2]][:,1], c="black", zorder=1)
 
-ax.scatter(Y[:,0], Y[:,1], c=color, cmap=plt.cm.Spectral, s=p_rad_2d**2)
+ax.scatter(Y[:,0], Y[:,1], c=color, cmap=plt.cm.Spectral, s=p_rad_2d**2, zorder=10)
+plt.show()
+
+# Triangles in the 3D plot
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection="3d")
+
+for simplex in tri.simplices:
+	points = X[simplex]
+	ax.plot(points[[0,1]][:,0], points[[0,1]][:,1], points[[0,1]][:,2], c="black", zorder=1)
+	ax.plot(points[[0,2]][:,0], points[[0,2]][:,1], points[[0,2]][:,2], c="black", zorder=1)
+	ax.plot(points[[1,2]][:,0], points[[1,2]][:,1], points[[1,2]][:,2], c="black", zorder=1)
+
+ax.scatter(X[:,0], X[:,1], X[:,2], c=color, cmap=plt.cm.Spectral, s=p_rad_3d**2)
+ax.view_init(4, -72)
 plt.show()
