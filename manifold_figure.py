339436083f87770264bd962c8f38074c92ce03fb
tommy <cohnt@umich.edu>
2020-05-10 00:03:47 -0400

    Move parameters to their own section. Always maximize the displayed window. Larger font size.

--- manifold_figure.py
+++ manifold_figure.py
@@ -5,15 +5,13 @@ from matplotlib.patches import Polygon
 from matplotlib.collections import PatchCollection
 from mpl_toolkits.mplot3d import Axes3D
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+import matplotlib
 from scipy.spatial import Delaunay
 
-# Dataset 3D plot
+# Params
 
 n_points = 250
-X, c = datasets.make_s_curve(n_points, random_state=1)
-c_max = np.max(c)
-c_min = np.min(c)
-color = (c - c_min) / (c_max - c_min)
+
 n_neighbors = 8
 n_components = 2
 
@@ -21,12 +19,25 @@ p_rad_3d = 18
 p_rad_2d = 25
 ew_3d = 3
 ew_2d = 5
+matplotlib.rcParams.update({'font.size': 32})
+
+def plt_show_fullsize():
+	mng = plt.get_current_fig_manager()
+	mng.resize(*mng.window.maxsize())
+	plt.show()
+
+# Dataset 3D plot
+
+X, c = datasets.make_s_curve(n_points, random_state=1)
+c_max = np.max(c)
+c_min = np.min(c)
+color = (c - c_min) / (c_max - c_min)
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection="3d")
 ax.scatter(X[:,0], X[:,1], X[:,2], c=color, cmap=plt.cm.Spectral, s=p_rad_3d**2, edgecolors="black", lw=ew_3d)
 ax.view_init(15, -72)
-plt.show()
+plt_show_fullsize()
 
 # Embedding plot
 
@@ -34,7 +45,7 @@ Y = manifold.Isomap(n_neighbors, n_components).fit_transform(X)
 fig = plt.figure()
 ax = fig.add_subplot(111)
 ax.scatter(Y[:,0], Y[:,1], c=color, cmap=plt.cm.Spectral, s=p_rad_2d**2, edgecolors="black", lw=ew_2d)
-plt.show()
+plt_show_fullsize()
 
 # Compute Delaunay
 
@@ -62,7 +73,7 @@ for simplex in tri.simplices:
 				ax.plot(points[[1,2]][:,0], points[[1,2]][:,1], c="black")
 
 ax.scatter(Y[:,0], Y[:,1], c=color, cmap=plt.cm.Spectral, s=p_rad_2d**2, zorder=10, edgecolors="black", lw=ew_2d)
-plt.show()
+plt_show_fullsize()
 
 # Triangles in the 3D plot
 
@@ -84,4 +95,4 @@ for simplex in tri.simplices:
 
 ax.scatter(X[:,0], X[:,1], X[:,2], c=color, cmap=plt.cm.Spectral, s=p_rad_3d**2, edgecolors="black", lw=ew_3d)
 ax.view_init(15, -72)
-plt.show()
+plt_show_fullsize()
