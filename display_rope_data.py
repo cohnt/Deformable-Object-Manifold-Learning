114929b8f1908921c28546b60f467120133d8876
Thomas Cohn <cohnt@umich.edu>
2020-06-24 13:15:15 -0400

    Set constant axes.

--- display_rope_data.py
+++ display_rope_data.py
@@ -10,6 +10,10 @@ with open("data/rope_3d_dataset.npy", "rb") as f:
 
 data_normalized = data[:,:,:] - np.repeat(data[:,0,:].reshape(data.shape[0], 1, data.shape[2]), data.shape[1], axis=1)
 
+x_lims = (np.min(data[:,:,0]), np.max(data[:,:,0]))
+y_lims = (np.min(data[:,:,1]), np.max(data[:,:,1]))
+z_lims = (np.min(data[:,:,2]), np.max(data[:,:,2]))
+
 plt.ion()
 
 fig = plt.figure()
@@ -17,6 +21,12 @@ ax = p3.Axes3D(fig)
 
 line = ax.plot(data_normalized[1,:,0], data_normalized[1,:,1], data_normalized[1,:,2])
 
+def set_lims():
+	ax.set_xlim(x_lims)
+	ax.set_ylim(y_lims)
+	ax.set_zlim(z_lims)
+
+set_lims()
 fig.canvas.draw()
 plt.show(block=False)
 
@@ -25,5 +35,6 @@ for i in range(2, data_normalized.shape[0]):
 	ax.clear()
 	line = ax.plot(data_normalized[i,:,0], data_normalized[i,:,1], data_normalized[i,:,2])
 
+	set_lims()
 	fig.canvas.draw()
 	time.sleep(0.01)
