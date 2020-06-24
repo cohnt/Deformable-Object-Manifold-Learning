9b06c7e1ecf20d8062e81f76f66ee23afcedf768
Thomas Cohn <cohnt@umich.edu>
2020-06-24 14:05:42 -0400

    Rotate the data.

--- display_rope_data.py
+++ display_rope_data.py
@@ -8,7 +8,24 @@ import matplotlib.animation as animation
 with open("data/rope_3d_dataset.npy", "rb") as f:
 	data = np.load(f)
 
-data_normalized = data[:,:,:] - np.repeat(data[:,0,:].reshape(data.shape[0], 1, data.shape[2]), data.shape[1], axis=1)
+data_centered = data[:,:,:] - np.repeat(data[:,0,:].reshape(data.shape[0], 1, data.shape[2]), data.shape[1], axis=1)
+
+data_rotated = np.zeros(data_centered.shape)
+for i in range(len(data_centered)):
+	# https://math.stackexchange.com/a/476311
+	a = data_centered[i,1,:]
+	b = np.array([1, 0, 0])
+	v = np.cross(a, b)
+	s = np.linalg.norm(v)
+	c = np.dot(a, b)
+	vx = np.array([
+		[0, -v[2], v[1]],
+		[v[2], 0, -v[0]],
+		[-v[1], v[0], 0]
+	])
+	R = np.eye(3) + vx + np.matmul(vx, vx)*(1 / (1+c))
+	for j in range(len(data_centered[i])):
+		data_rotated[i,j,:] = np.matmul(R, data_centered[i,j,:])
 
 x_lims = (np.min(data[:,:,0]), np.max(data[:,:,0]))
 y_lims = (np.min(data[:,:,1]), np.max(data[:,:,1]))
@@ -19,7 +36,7 @@ plt.ion()
 fig = plt.figure()
 ax = p3.Axes3D(fig)
 
-line = ax.plot(data_normalized[1,:,0], data_normalized[1,:,1], data_normalized[1,:,2])
+line = ax.plot(data_rotated[1,:,0], data_rotated[1,:,1], data_rotated[1,:,2])
 
 def set_lims():
 	ax.set_xlim(x_lims)
@@ -30,10 +47,10 @@ set_lims()
 fig.canvas.draw()
 plt.show(block=False)
 
-for i in range(2, data_normalized.shape[0]):
+for i in range(2, data_rotated.shape[0]):
 
 	ax.clear()
-	line = ax.plot(data_normalized[i,:,0], data_normalized[i,:,1], data_normalized[i,:,2])
+	line = ax.plot(data_rotated[i,:,0], data_rotated[i,:,1], data_rotated[i,:,2])
 
 	set_lims()
 	fig.canvas.draw()
