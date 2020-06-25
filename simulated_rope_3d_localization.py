61c605908353fbfa0ac95c5b5fae286730064f1c
Thomas Cohn <cohnt@umich.edu>
2020-06-25 15:54:31 -0400

    Be harsh to particles that leave the bounding box.

--- simulated_rope_3d_localization.py
+++ simulated_rope_3d_localization.py
@@ -192,7 +192,8 @@ class Particle():
 			if (0 <= heatmap_index).all() and (heatmap_index < heatmap_shape).all():
 				running_total += heatmap[tuple(heatmap_index)]
 			else:
-				continue
+				self.raw_weight = 0
+				return self.raw_weight
 
 		self.raw_weight = running_total
 		return self.raw_weight
