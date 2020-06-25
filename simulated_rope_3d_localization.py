9d82a4b3cc5bd93e4868e25877ddc962933d0ab5
Thomas Cohn <cohnt@umich.edu>
2020-06-25 12:46:49 -0400

    Trying to localize using the simulated rope training dataset.

---
+++ simulated_rope_3d_localization.py
@@ -0,0 +1,97 @@
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+import mpl_toolkits.mplot3d.axes3d as p3
+
+# Load the data
+with open("data/rope_3d_dataset.npy", "rb") as f:
+	data = np.load(f)
+
+data_centered = data[:,:,:] - np.repeat(data[:,0,:].reshape(data.shape[0], 1, data.shape[2]), data.shape[1], axis=1)
+
+data_rotated = np.zeros(data_centered.shape)
+for i in range(len(data_centered)):
+	# https://math.stackexchange.com/a/476311
+	a = data_centered[i,-1,:] / np.linalg.norm(data_centered[i,-1,:])
+	b = np.array([1, 0, 0])
+	v = np.cross(a, b)
+	s = np.linalg.norm(v)
+	c = np.dot(a, b)
+	vx = np.array([
+		[0, -v[2], v[1]],
+		[v[2], 0, -v[0]],
+		[-v[1], v[0], 0]
+	])
+	R = np.eye(3) + vx + np.dot(vx, vx)*(1 / (1+c))
+	for j in range(len(data_centered[i])):
+		data_rotated[i,j,:] = np.matmul(R, data_centered[i,j,:])
+
+train = data_rotated[0:1000].reshape(1000,-1)
+
+
+from sklearn.manifold import Isomap
+
+embedding = Isomap(n_neighbors=12, n_components=2).fit_transform(train)
+
+fig = plt.figure()
+ax0 = fig.add_subplot(1, 2, 1)
+ax1 = fig.add_subplot(1, 2, 2, projection="3d")
+axes = [ax0, ax1]
+
+points = axes[0].scatter(embedding[:,0], embedding[:,1], c="grey", s=20**2)
+xlim = axes[0].get_xlim()
+ylim = axes[0].get_ylim()
+
+
+from scipy.spatial import Delaunay
+
+interpolator = Delaunay(embedding, qhull_options="QJ")
+
+################
+# Display Plot #
+################
+
+def hover(event):
+	xy = np.array([event.xdata, event.ydata])
+
+	# Check if xy is in the convex hull
+	simplex_num = interpolator.find_simplex(xy)
+	# print "xy", xy, "\tsimplex_num", simplex_num
+	if simplex_num != -1:
+		# Get the simplex
+		simplex_indices = interpolator.simplices[simplex_num]
+		# print "simplex_indices", simplex_indices
+		simplex = interpolator.points[simplex_indices]
+		# print "simplex", simplex
+
+		# Display the simplex vertices
+		axes[0].clear()
+		axes[0].scatter(embedding[:,0], embedding[:,1], c="grey", s=20**2)
+		axes[0].scatter(embedding[simplex_indices,0], embedding[simplex_indices,1], c="blue", s=20**2)
+		axes[0].plot(embedding[simplex_indices[[0,1]],0], embedding[simplex_indices[[0,1]],1], c="blue", linewidth=3)
+		axes[0].plot(embedding[simplex_indices[[1,2]],0], embedding[simplex_indices[[1,2]],1], c="blue", linewidth=3)
+		axes[0].plot(embedding[simplex_indices[[0,2]],0], embedding[simplex_indices[[0,2]],1], c="blue", linewidth=3)
+		axes[0].set_xlim(xlim)
+		axes[0].set_ylim(ylim)
+
+		# Compute barycentric coordinates
+		A = np.vstack((simplex.T, np.ones((1, 3))))
+		b = np.vstack((xy.reshape(-1, 1), np.ones((1, 1))))
+		b_coords = np.linalg.solve(A, b)
+		b = np.asarray(b_coords).flatten()
+		print "b_coords", b, np.sum(b_coords)
+
+		# Interpolate the deformation
+		mult_vec = np.zeros(len(train))
+		mult_vec[simplex_indices] = b
+		curve = np.sum(np.matmul(np.diag(mult_vec), train), axis=0).reshape(-1,3)
+		# print "curve", curve
+		axes[1].clear()
+		axes[1].plot(curve[:,0], curve[:,1], curve[:,2])
+
+		fig.canvas.draw_idle()
+
+fig.canvas.mpl_connect('motion_notify_event', hover)
+mng = plt.get_current_fig_manager()
+mng.resize(*mng.window.maxsize())
+plt.show()
