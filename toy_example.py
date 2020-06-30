b458a1b08c01f12c19b5cf2510b58ab658818ba2
Thomas Cohn <cohnt@umich.edu>
2020-06-30 17:44:21 -0400

    Go to precisely 50 dimensions.

--- toy_example.py
+++ toy_example.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import mpl_toolkits.mplot3d.axes3d as p3
 
-extra_dims = 50
+extra_dims = 47
 max_iters = 200
 predict_mode = "average" # mle or average
 norm = 2
