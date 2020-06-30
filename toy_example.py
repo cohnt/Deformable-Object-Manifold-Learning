f75d21d719d48b70c01b75152c0e3896bcef5527
Thomas Cohn <cohnt@umich.edu>
2020-06-30 12:35:41 -0400

    Define the likelihood function.

--- toy_example.py
+++ toy_example.py
@@ -8,4 +8,7 @@ actual = np.array([0.5, 0.5])
 from sklearn.manifold import Isomap
 ism = Isomap(n_neighbors=5, n_components=1)
 embedding = ism.fit_transform(data)
-actual_embedded = ism.transform([actual])[0]
+
+from scipy.stats import multivariate_normal
+def likelihood(point):
+	return multivariate_normal.pdf(point, mean=actual, cov=np.eye(2))
