850e5f8be3d220a056127bc507705bfd53710fc2
Thomas Cohn <cohnt@umich.edu>
2020-07-13 11:47:37 -0400

    Also load the test dataset.

--- mouse_localization.py
+++ mouse_localization.py
@@ -27,10 +27,29 @@ def xyz2uvd(jnt):
 		return np.array([u,v,z])
 
 # Load training data
-depths = []
-train = []
+train_depths = []
+train_xyz = []
+train_uvd = []
 for i in range(n_train):
 	print filepath + ("train/%05i.png" % (i+1))
 	depth = np.array(PIL.Image.open(filepath + ("train/%05i.png" % (i+1))))
 	xyz = np.loadtxt(filepath + ("train/%05i.txt" % (i+1)))
 	uvd = xyz2uvd(xyz)
+
+	train_depths.append(depth)
+	train_xyz.append(xyz)
+	train_uvd.append(uvd)
+
+# Load testing data
+test_depths = []
+test_xyz = []
+test_uvd = []
+for i in range(n_test):
+	print filepath + ("test/%05i.png" % (i+1))
+	depth = np.array(PIL.Image.open(filepath + ("test/%05i.png" % (i+1))))
+	xyz = np.loadtxt(filepath + ("test/%05i.txt" % (i+1)))
+	uvd = xyz2uvd(xyz)
+
+	test_depths.append(depth)
+	test_xyz.append(xyz)
+	test_uvd.append(uvd)
