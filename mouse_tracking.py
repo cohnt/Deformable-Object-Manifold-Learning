7bf727ac10e5fa3abc238cb03c3a558aa9b84e7f
Thomas Cohn <cohnt@umich.edu>
2020-07-13 14:39:17 -0400

    Keep the MLE.

--- mouse_tracking.py
+++ mouse_tracking.py
@@ -343,7 +343,8 @@ for frame in range(test_start_ind, n_test):
 	step = 1/float((n_particles * (1-exploration_factor))+1)
 	chkVal = step
 	chkIdx = 0
-	for i in range(int(np.ceil(n_particles * (1-exploration_factor)))):
+	newParticles.append(particles[max_normalized_weight_ind])
+	for i in range(1, int(np.ceil(n_particles * (1-exploration_factor)))):
 		while cs[chkIdx] < chkVal:
 			chkIdx = chkIdx + 1
 		chkVal = chkVal + step
