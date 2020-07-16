16ffd4d52f6b146776d4829c56ade58d61789233
Thomas Cohn <cohnt@umich.edu>
2020-07-16 17:15:51 -0400

    Display the MLE on top of everything else.

--- pmpnbp_2d_example.py
+++ pmpnbp_2d_example.py
@@ -360,9 +360,9 @@ while True:
 	draw_scene(ax)
 	for p in particles:
 		p.draw(ax)
-	ax.scatter([particles[max_normalized_weight_ind].circle.position[0]], [particles[max_normalized_weight_ind].circle.position[1]], color="red")
+	ax.scatter([particles[max_normalized_weight_ind].circle.position[0]], [particles[max_normalized_weight_ind].circle.position[1]], color="red", zorder=2)
 	for rectangle in particles[max_normalized_weight_ind].rectangles:
-		ax.scatter(rectangle.get_vertices()[:,0], rectangle.get_vertices()[:,1], color="red")
+		ax.scatter(rectangle.get_vertices()[:,0], rectangle.get_vertices()[:,1], color="red", zorder=2)
 	plt.draw()
 	plt.pause(0.001)
 
