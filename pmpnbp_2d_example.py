403ddfe7c9388e27cc3c6531e9b2ed3c2af86f53
Thomas Cohn <cohnt@umich.edu>
2020-07-16 13:22:01 -0400

    Actually draw the scene.

--- pmpnbp_2d_example.py
+++ pmpnbp_2d_example.py
@@ -19,12 +19,13 @@ n_rectangles = 20
 # Types #
 #########
 
+def rad2deg(r):
+	return r * 180 / np.pi
+
 class Circle():
 	def __init__(self, position=None, radius=None):
 		if position is None:
-			self.position = np.array([
-				np.random.uniform([0, 0], dims)
-			])
+			self.position = np.random.uniform([0, 0], dims)
 		else:
 			self.position = position
 
@@ -33,12 +34,14 @@ class Circle():
 		else:
 			self.radius = radius
 
+	def draw(self, ax, color="white"):
+		circle = plt.Circle(self.position, radius=self.radius, facecolor=color)
+		ax.add_patch(circle)
+
 class Rectangle():
 	def __init__(self, position=None, orientation=None, size=None):
 		if position is None:
-			self.position = np.array([
-				np.random.uniform([0, 0], dims)
-			])
+			self.position = np.random.uniform([0, 0], dims)
 		else:
 			self.position = position
 
@@ -52,6 +55,10 @@ class Rectangle():
 		else:
 			self.size = size
 
+	def draw(self, ax, color="white"):
+		rectangle = plt.Rectangle(self.position, self.size[0], self.size[1], angle=rad2deg(self.orientation), facecolor=color)
+		ax.add_patch(rectangle)
+
 ####################
 # Create the Scene #
 ####################
@@ -62,3 +69,13 @@ for _ in range(n_circles):
 	circles.append(Circle())
 for _ in range(n_rectangles):
 	rectangles.append(Rectangle())
+
+fig, ax = plt.subplots(1, 1)
+ax.set_xlim((0, dims[0]))
+ax.set_ylim((0, dims[1]))
+ax.set_facecolor("black")
+for circle in circles:
+	circle.draw(ax)
+for rectangle in rectangles:
+	rectangle.draw(ax)
+plt.show()
