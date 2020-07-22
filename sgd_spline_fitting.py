1fbc1c4240d88d58888f20f7589ac650f5c851b3
Thomas Cohn <cohnt@umich.edu>
2020-07-22 15:26:50 -0400

    Make CatmulRomSplineSegment callable.

--- sgd_spline_fitting.py
+++ sgd_spline_fitting.py
@@ -25,11 +25,33 @@ angles = np.linspace(0, 2*np.pi, spline_n_points+1)[0:-1]
 control_points = np.vstack((np.cos(angles), np.sin(angles))).T
 
 class CatmulRomSplineSegment():
-	def __init__(self, P0, P1, P2, P3):
+	def __init__(self, P0, P1, P2, P3, alpha=0.5):
 		self.P0 = P0
 		self.P1 = P1
 		self.P2 = P2
 		self.P3 = P3
+		self.alpha = alpha
+
+		def tj(ti, Pi, Pj):
+			xi, yi = Pi
+			xj, yj = Pj
+			return ((xj-xi)**2 + (yj-yi)**2)**alpha + ti
+
+		self.t0 = 0
+		self.t1 = tj(self.t0, self.P0, self.P1)
+		self.t2 = tj(self.t1, self.P1, self.P2)
+		self.t3 = tj(self.t2, self.P2, self.P3)
+
+	def __call__(self, T):
+		# T should be in [0, 1], where 0 maps to P1 and 1 maps to P2
+		t = ((self.t2 - self.t1) * T) + self.t1
+		A1 = (self.t1-t)/(self.t1-self.t0)*self.P0 + (t-self.t0)/(self.t1-self.t0)*self.P1
+		A2 = (self.t2-t)/(self.t2-self.t1)*self.P1 + (t-self.t1)/(self.t2-self.t1)*self.P2
+		A3 = (self.t3-t)/(self.t3-self.t2)*self.P2 + (t-self.t2)/(self.t3-self.t2)*self.P3
+		B1 = (self.t2-t)/(self.t2-self.t0)*A1 + (t-self.t0)/(self.t2-self.t0)*A2
+		B2 = (self.t3-t)/(self.t3-self.t1)*A2 + (t-self.t1)/(self.t3-self.t1)*A3
+		C = (self.t2-t)/(self.t2-self.t1)*B1 + (t-self.t1)/(self.t2-self.t1)*B2
+		return C
 
 class CatmulRomSpline():
 	def __init__(self, points):
