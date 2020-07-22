531b003535beb2ddfdb4d0295c5e1cffe5227926
Thomas Cohn <cohnt@umich.edu>
2020-07-22 18:00:22 -0400

    More spline points.

--- sgd_spline_fitting.py
+++ sgd_spline_fitting.py
@@ -48,7 +48,7 @@ blob_max_y = blob_min_y + blob.shape[1] + 1
 mask[blob_min_x:blob_min_x+blob.shape[0], blob_min_y:blob_min_y+blob_size[1]] = blob
 blob_points = np.flip(np.transpose(blob.nonzero()), axis=1) +  [blob_min_x, blob_min_y]
 
-spline_n_points = 8
+spline_n_points = 20
 spline_init_radius = 10
 centroid = np.flip(np.array(center_of_mass(mask)))
 angles = np.linspace(0, 2*np.pi, spline_n_points+1)[0:-1]
