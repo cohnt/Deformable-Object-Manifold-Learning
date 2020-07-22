1531a7938be27f129e36f8b06718e21b6e88dc93
Thomas Cohn <cohnt@umich.edu>
2020-07-22 15:15:51 -0400

    Beginning to setup the spline classes.

--- sgd_spline_fitting.py
+++ sgd_spline_fitting.py
@@ -23,3 +23,19 @@ spline_n_points = 8
 centroid = np.array(center_of_mass(mask))
 angles = np.linspace(0, 2*np.pi, spline_n_points+1)[0:-1]
 control_points = np.vstack((np.cos(angles), np.sin(angles))).T
+
+class CatmulRomSplineSegment():
+	def __init__(self, P0, P1, P2, P3):
+		self.P0 = P0
+		self.P1 = P1
+		self.P2 = P2
+		self.P3 = P3
+
+class CatmulRomSpline():
+	def __init__(self, points):
+		self.segments = []
+		self.segments.append(CatmulRomSplineSegment(points[-1], points[0], points[1], points[2]))
+		for i in range(len(points) - 3):
+			self.segments.append(CatmulRomSplineSegment(points[i], points[i+1], points[i+2], points[i+3]))
+		self.segments.append(CatmulRomSplineSegment(points[-3], points[-2], points[-1], points[0]))
+		self.segments.append(CatmulRomSplineSegment(points[-2], points[-1], points[0], points[1]))
