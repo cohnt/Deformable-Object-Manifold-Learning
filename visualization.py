a00a8450803f02767a146bcf3c4ad6c04859e433
Thomas Cohn <cohnt@umich.edu>
2020-12-29 14:49:44 -0500

    Slightly better instructions for how to use this code.

--- visualization.py
+++ visualization.py
@@ -6,4 +6,6 @@ def maximize_window():
 	mng.resize(*mng.window.maxsize())
 
 def combine_images_to_video(fname_format="iteration\%03d.png"):
+	# fname_format should be something the command line tool ffmpeg can understand
+	# For example, iteration\%03d.png expects images with filenames ieration001.png, iteration002.png, etc.
 	os.system('ffmpeg -f image2 -r 1/0.1 -i %s -c:v libx264 -pix_fmt yuv420p out.mp4' % fname_format)
