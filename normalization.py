1b6170e9c8eb8dd7bf58b64e89be7cae51348877
Thomas Cohn <cohnt@umich.edu>
2020-12-29 13:20:23 -0500

    Implement function to uniformly center pointclouds.

--- normalization.py
+++ normalization.py
@@ -9,7 +9,6 @@ def normalize_pointcloud_2d(data, centering_idx=0, orientation_idx=1):
 	# centering_idx is the index of the point which is centered at 0
 	# orientation_idx is the index of the point used for uniform rotating
 	# These indices must be distinct
-	# Returns (normalized_data, inverse_transformation_matrix)
 	pass
 
 def normalize_pointcloud_3d(data, centering_idx=0, orientation_idx_1=1, orientation_idx_2=2):
@@ -19,7 +18,6 @@ def normalize_pointcloud_3d(data, centering_idx=0, orientation_idx_1=1, orientat
 	# centering_idx is the index of the point which is centered at 0
 	# orientation_idx_1, orientation_idx_2 are the indices of the points used for uniform rotating
 	# These indices must be distinct, and should be chosen so that they aren't collinear
-	# Returns (normalized_data, inverse_transformation_matrix)
 	pass
 
 def center_pointcloud(data, centering_idx=0):
@@ -28,5 +26,6 @@ def center_pointcloud(data, centering_idx=0):
 	# n_points is the number of points in a point cloud
 	# n_dimensions is arbitrary
 	# centering_idx is the index of the point which is centered at 0
-	# Returns (centered_data, inverse_transformation_matrix)
-	pass
+	offset = data[:,[centering_idx],:] # Shape (n_clouds, 1, n_dimension)
+	offset_cloud = np.tile(offset, (1, data.shape[1], 1)) # Shape (n_clouds, n_points, n_dimensions)
+	return data - offset_cloud
