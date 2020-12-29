ae5efca49e20b0e089915ce0f021ff4ef1cf5984
Thomas Cohn <cohnt@umich.edu>
2020-12-29 14:43:41 -0500

    Function to combine image files (with a standard naming format) to video.

--- visualization.py
+++ visualization.py
@@ -4,3 +4,6 @@ import matplotlib.pyplot as plt
 def maximize_window():
 	mng = plt.get_current_fig_manager()
 	mng.resize(*mng.window.maxsize())
+
+def combine_images_to_video(fname_format="iteration\%03d.png"):
+	os.system('ffmpeg -f image2 -r 1/0.1 -i %s -c:v libx264 -pix_fmt yuv420p out.mp4' % fname_format)
