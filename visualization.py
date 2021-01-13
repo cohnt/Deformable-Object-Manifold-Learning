ecf10f8c0de3bd708672e90c6f1bda319878ebe1
Thomas Cohn <cohnt@umich.edu>
2021-01-13 09:03:03 -0500

    Make combine_images_to_video work for directories.

--- visualization.py
+++ visualization.py
@@ -6,10 +6,10 @@ def maximize_window():
 	mng = plt.get_current_fig_manager()
 	mng.resize(*mng.window.maxsize())
 
-def combine_images_to_video(fname_format="iteration\%03d.png"):
+def combine_images_to_video(fname_format="iteration\%03d.png", save_file="out.mp4"):
 	# fname_format should be something the command line tool ffmpeg can understand
 	# For example, iteration\%03d.png expects images with filenames ieration001.png, iteration002.png, etc.
-	os.system('ffmpeg -f image2 -r 1/0.1 -i %s -c:v libx264 -pix_fmt yuv420p out.mp4' % fname_format)
+	os.system('ffmpeg -f image2 -r 1/0.1 -i %s -c:v libx264 -pix_fmt yuv420p %s' % (fname_format, save_file))
 
 def create_interactive_embedding_visulization(cc, point_cloud_dim):
 	fig = plt.figure()
